555bca64825a7f715f35c026880c07917c53bace
Claudiu Filip <claudiucelfilip@gmail.com>
2019-10-22 11:23:49 +0300

    removed slower sourcemap configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/main.js",
-  devtool: "inline-cheap-module-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "wavelet-client.umd.js",
