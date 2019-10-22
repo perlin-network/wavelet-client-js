c7b97852cc2b212858607671a040a27dc6c83c44
Claudiu Filip <claudiucelfilip@gmail.com>
2019-10-22 11:17:27 +0300

    change TextDecoder fallback and added babel/core-js transformations for olde browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/main.js',
-    
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'wavelet-client.umd.js',
-        library: 'wavelet-client',
-        libraryTarget: 'umd',
-        libraryExport: "default" ,
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
-    },
-    mode: 'development',
-    resolve: {
-        alias: {
-            // websocket: path.resolve(__dirname, 'node_modules/websocket/lib/websocket.js'),
-            atob: path.resolve(__dirname, 'node_modules/atob/node-atob.js'), 
-        }
+  entry: "./src/main.js",
+  devtool: "inline-cheap-module-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "wavelet-client.umd.js",
+    library: "wavelet-client",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    globalObject: `(typeof self !== 'undefined' ? self : this)`
+  },
+  mode: "development",
+  resolve: {
+    alias: {
+      // websocket: path.resolve(__dirname, 'node_modules/websocket/lib/websocket.js'),
+      atob: path.resolve(__dirname, "node_modules/atob/node-atob.js")
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
 };
